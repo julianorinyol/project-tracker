e777e78c3c0d7d5c9fdc0ab25879dff4e89aca2c
JulianOrinyol <julianorinyol@gmail.com>
2015-10-14 13:10:32 -0700

    set up association btw projects and organizations

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,4 @@
 class Organization < ActiveRecord::Base
   belongs_to :leader
+  has_many :projects
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151014195157) do
+ActiveRecord::Schema.define(version: 20151014200659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,9 @@ ActiveRecord::Schema.define(version: 20151014195157) do
   end
 
   create_table "projects", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.integer  "organization_id"
   end
 
   create_table "projects_users", force: :cascade do |t|
