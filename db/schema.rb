49caa5c51d36f1e512e29a3de1c242c9882c439b
JulianOrinyol <julianorinyol@gmail.com>
2015-10-14 11:26:14 -0700

    added student and leader models, setting up single table inheritence

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151014174012) do
+ActiveRecord::Schema.define(version: 20151014181709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "organizations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
---
+++ spec/factories/leaders.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :leader do
+    
+  end
+
+end
