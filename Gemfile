97a4e6f82a88c3fc686c95efcfc231de9fcdcc29
JulianOrinyol <julianorinyol@gmail.com>
2015-10-14 13:51:23 -0700

    added shoulda-matchers config to rails-helper..  wrote specs for project

--- Gemfile
+++ Gemfile
@@ -36,4 +36,5 @@ group :test do
   gem 'database_cleaner'
   gem 'launchy'
   gem 'selenium-webdriver'
+  gem 'shoulda-matchers'
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+
+
+  # #*********************************Validations**********************************************************# 
+  it { is_expected.to validate_presence_of :organization_id }
+  it { is_expected.to validate_presence_of :name }
+  it { is_expected.to validate_presence_of :description }
+
+
+
+  #     create_table "projects", force: :cascade do |t|
+  #   t.datetime "created_at",      null: false
+  #   t.datetime "updated_at",      null: false
+  #   t.integer  "organization_id"
+  # end
+
+  # #*********************************Associations**********************************************************# 
+
+  it { is_expected.to belong_to(:organization) }
+  it { is_expected.to have_many(:milestones) }                                                                                                                
+  it { is_expected.to have_many(:content_pieces) }
+  it { is_expected.to have_and_belong_to_many(:students) }
+
+
+
 end
