b0566979d7a9cb18b0a9d3b4f3461ecb6ed9bf6b
JulianOrinyol <julianorinyol@gmail.com>
2015-10-14 13:11:41 -0700

    added scaffold for milestones

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :milestones
   resources :organizations
   resources :projects
   root to: 'visitors#index'
---
+++ spec/factories/milestones.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :milestone do
+    
+  end
+
+end
