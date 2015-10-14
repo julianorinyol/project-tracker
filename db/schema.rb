13903b71e24227dc826af2ca0bae54070198fdd9
JulianOrinyol <julianorinyol@gmail.com>
2015-10-14 11:27:14 -0700

    migrated user table to support single table inheritence

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151014181709) do
+ActiveRecord::Schema.define(version: 20151014182632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20151014181709) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "role"
+    t.string   "type"
   end
 
 end
